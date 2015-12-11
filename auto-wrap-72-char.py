surabhigupta/AutoWrapSeventyTwo

commit 08eafe09227a64acebdf59a7fce4b451eafc6da2

Make improvements to the script and add documentation

auto-wrap-72-char.py
@@ -1,75 +1,99 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+import codecs
+
 import sys
 
 debug = False
 
-def wrapCommitMessageTo72CharsPerLine(message):
-    """
-    Given a commit message, add line breaks at appropriate places such that 
-    no message is more than 72 characters wide.
-    Exception: URLs or other long strings that are more than 72 characters wide (since these cannot be broken up)
 
-    The algorithm below tracks the position of one previous whitespace character and maintains
-    a running count of characters since last new line. After the count exceeds 72, the next whitespace
-    is converted into a new line character \r\n (carriage return/line feed) 
+def info(message):
+    if debug:
+        print(message, file=sys.stderr)
+        sys.stderr.flush()
+
+
+def wrapCommitMessageToAMaxWidth(originalMessage, maxWidth=72, linesep='\n'):
+    """
+    Given a commit message, add line breaks at appropriate places such that no line
+    in the message is more than 72 characters wide.
+
+    :param originalMessage:
+    :param linesep: line separator that is inserted into the message
+    :param message: a file object representing the commit message
+    :param maxWidth: the maximum width for the message
+
+    Exception: URLs or other long strings that are more than 72 characters wide
+    (since these cannot be broken up)
+
+    The algorithm below tracks the position of one previous whitespace character
+    and maintains a running count of characters since the last new line. If the count
+    exceeds 72, the next whitespace is replaced by a (os-specific) line separator
 
     Existing line breaks are left as-is, as are any lines that are less than 72 characters long.
+    Note: There exists a "textwrap" library python that will wrap commit messages
+    to a specified width. However, it truncates in the middle of an url (or a long string)
+    if its length is more than the max width. I welcome any suggestions and/or contributions
+    around performance improvements.
     """
 
     #The formatted commit message
-    commit_msg = ""
+    commit_msg = []
+    # Helper method to replace the whitespace at specific positions in the
+    # last line of the commit message with a new line character.
+    # The last element of the commit_msg array is replaced by the two elements,
+    # the first of which ends with a line separator
+
+
+    def replaceWhiteSpaceWithNewLine(position):
+        currentLine = commit_msg.pop()
+        
+        #Throw warning if white space is being inserted within the boundaries
+        #of contiguous text
+        if not currentLine[position-1].isspace():
+            info("Warning: White space expected at position %d %s" % (position, currentLine[position]))
+
+        #White space is replaced by a new line character at the specified position   
+        commit_msg.append(currentLine[:(position-1)] + linesep)
+        commit_msg.append(currentLine[position:])
 
     #Going through all the lines in the commit message, one line at a time
-    for lineno, line in enumerate(message):
-        characterCountSinceNewLine = 0;
-        lastWhiteSpace=0
+    for lineno, line in enumerate(originalMessage):
+        characterCountSinceNewLine = 0
+        lastWhiteSpace = 0
+        commit_msg.append(line)
 
-        for index in range(0, len(line)):
-            commit_msg += line[index]
+        for index, ch in enumerate(line):
             characterCountSinceNewLine += 1
-            if (debug) :
-                print "Character: " + str(line[index]) + " count: " + str(characterCountSinceNewLine)
-            if line[index].isspace():
-                if characterCountSinceNewLine > 72 and lastWhiteSpace > 0:
-                    if (debug) :
-                        print "Inserting a new line after previous word " + str(index-lastWhiteSpace+1) + " characters ago."
-                        print "Resetting character count since last new line to " + str(index - lastWhiteSpace)
-                    commit_msg = replaceWhiteSpaceWithNewLine(commit_msg, -(index-lastWhiteSpace+1))
-                    characterCountSinceNewLine = index-lastWhiteSpace
+            if ch.isspace():
+                info("Character count since last new line: %d" % characterCountSinceNewLine)
+                if characterCountSinceNewLine > (maxWidth + 1) and lastWhiteSpace > 0:
+                    replaceWhiteSpaceWithNewLine(lastWhiteSpace)
+                    characterCountSinceNewLine = (characterCountSinceNewLine - lastWhiteSpace)
+                    lastWhiteSpace = 0
+                    info("Inserting a new line after previous word:%s (%d characters ago)" %
+                         (line[lastWhiteSpace:characterCountSinceNewLine], characterCountSinceNewLine))
+                    info("Resetting character count since last new line to %d" % characterCountSinceNewLine)
+
                 #last white space is updated
-                lastWhiteSpace=index;
-    return commit_msg
+                lastWhiteSpace = characterCountSinceNewLine
 
-# Helper method to replace the whitespace with new line character
-# at specific positions in the commit message. Note the usage of 
-# negative indices for better readability and accuracy (in cases
-# where the length of the commit message was modified)
-
-def replaceWhiteSpaceWithNewLine(commit_Message, position) :
-    if not commit_Message[position].isspace():
-        print "Warning: White space expected at position " + str(position)
-    
-    #A new line character is added at the specified position
-    result = commit_Message[:position] + '\r\n' 
-    if (position !=-1):
-        result += commit_Message[(position+1):] 
-    
-    return result
+    return ''.join(commit_msg) 
 
 # 
 # Begin main method
 #
 
-def main(args) :
+
+def main(args):
     message_file = args[1]
     #Process original commit message
-    with open(message_file) as originalCommit:
-        formatted_commit_msg = wrapCommitMessageTo72CharsPerLine(originalCommit)
+    with codecs.open(message_file, 'rb', encoding='utf-8') as originalCommit:
+        formatted_commit_msg = wrapCommitMessageToAMaxWidth(originalCommit)
 
     #Write formatted commit message
-    with open(message_file, 'w') as formattedMessageFile:
-        formattedMessageFile.write(formatted_commit_msg)
+    with codecs.open(message_file, 'wb', encoding='utf-8') as f
 
 #
 # End main method

commit-msg
@@ -1,3 +1,23 @@
+# A well formed git commit message should be within 72 columns for a few reasons:
+# 
+# (From http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html)
+#
+# -- git log doesn’t do any special special wrapping of the commit
+#    messages. With the default pager of less -S, this means your paragraphs
+#    flow far off the edge of the screen, making them difficult to read. On
+#    an 80 column terminal, if we subtract 4 columns for the indent on the
+#    left and 4 more for symmetry on the right, we’re left with 72
+#    columns.
+# -- git format-patch --stdout converts a series of commits to a series of
+#    emails, using the messages for the message body. Good email netiquette
+#    dictates we wrap our plain text emails such that there’s room for a
+#    few levels of nested reply indicators without overflow in an 80 column
+#    terminal. (The current rails.git workflow doesn’t include email, but
+#    who knows what the future will bring.)
+#
+# This script looks at each commit that is about to be pushed and adds new 
+# line characters as necessary.
+
 #!/bin/sh
 
 if [ "$GITAUTOWRAP" = "true" ]; then

commit a5bcefadca40154e3432a6951fbb73e72e27531e

Fix formatted message writing
08eafe09227a64acebdf59a7fce4b451eafc6da2 broke writing of the formatted
commit message (and by extension the whole script, because the
offending with statement was lacking a body).

auto-wrap-72-char.py
@@ -93,7 +93,8 @@ def main(args):
         formatted_commit_msg = wrapCommitMessageToAMaxWidth(originalCommit)
 
     #Write formatted commit message
-    with codecs.open(message_file, 'wb', encoding='utf-8') as f
+    with codecs.open(message_file, 'wb', encoding='utf-8') as formattedMessageFile:
+        formattedMessageFile.write(formatted_commit_msg)
 
 #
 # End main method